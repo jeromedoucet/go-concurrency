1f618660a8be883ff868df24fd18e85536d9f9f9
ortegadiana <dortega@xebia.fr>
2016-06-27 13:49:16 +0200

    return in logs when error

--- client/orderMaker.go
+++ client/orderMaker.go
@@ -1,29 +1,35 @@
 package client
 
 import (
-	"github.com/vil-coyote-acme/go-concurrency/commons"
-	"time"
-	"math/rand"
-	"strconv"
-	"github.com/garyburd/redigo/redis"
-	"encoding/json"
-	"net/http"
 	"bytes"
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+	"github.com/vil-coyote-acme/go-concurrency/commons"
 )
 
 func startNewOrderMaker(url string, rAddr string, reg commons.Registration, unRegChan chan commons.Registration) {
 	go func() {
 		r := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
-		c, _ := redis.Dial("tcp", rAddr)
+		c, erredis := redis.Dial("tcp", rAddr)
+		if erredis != nil {
+			log.Println(erredis.Error())
+			return
+		}
 		defer c.Close()
 		for {
 			id := int(time.Now().UTC().UnixNano())
 			t := r.Intn(6)
 			nb := r.Intn(21)
-			o := commons.Order{Id:id, Quantity:nb, Type:commons.BeverageType(t), CallBackUrl:url + "/bill/" + reg.PlayerId + "/" + strconv.Itoa(id)}
+			o := commons.Order{Id: id, Quantity: nb, Type: commons.BeverageType(t), CallBackUrl: url + "/bill/" + reg.PlayerId + "/" + strconv.Itoa(id)}
 			bdOrder, _ := json.Marshal(o)
 			c.Do("SET", o.Id, string(bdOrder))
-			resp, err := http.Post(reg.Ip + "/orders", "application/json", bytes.NewBuffer(bdOrder))
+			resp, err := http.Post(reg.Ip+"/orders", "application/json", bytes.NewBuffer(bdOrder))
 			if err != nil || resp.StatusCode != 200 {
 				unRegChan <- reg
 				return
